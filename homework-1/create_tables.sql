51ee349de89858156422ff9f352d956eb33c1ded
altarev_da <altarev_da@nlmk.com>
2023-05-12 14:56:38 +0300

    test

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees (
+	first_name VARCHAR(255),        --имя
+	last_name VARCHAR(255),         --фамилия
+	title VARCHAR(255) NOT NULL,    --должность
+	birth_date DATE                 --дата рождения
+	notes TEXT,
+    employer_id INT                   
+	PRIMARY KEY (employer_id)
+);
+
+CREATE TABLE customers (
+	customer_id VARCHAR(255),       --id_клиента
+	company_name VARCHAR(255),      --название компании
+	contact_name VARCHAR(255),      --имя для связи
+	PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE orders (
+	order_id INT PRIMARY KEY,                                           --id_заказа                                         
+	employee_id int REFERENCES employees(employer_id) NOT NULL,         --id_работника
+	order_date DATE,                                                    --дата заказа
+	ship_city VARCHAR(255)
+    customer_id VARCHAR(255) REFERENCES customers(customer_id) NOT NULL --id_клиента
+    
+);	
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,34 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+def r_file(file_name):
+    data_list = []
+    with open(f"C:\\Users\\79056\\Documents\\git\\17.1\\postgres-homeworks\\homework-1\\north_data\\{file_name}.csv", 
+              encoding='utf-8') as file:
+        file_reader = csv.reader(file, delimiter = ",")
+        for row in file_reader:
+            data_list.append(row)
+    print(data_list)
+    return data_list
+    
+
+# print("ТЕСТ_1")
+# test_1 = r_file("customers_data")
+# print("ТЕСТ_2")
+# test_2 = r_file("employees_data")
+# print("ТЕСТ_3")
+# TEST_3 = r_file("orders_data")
+
+# подключение к БД
+conn = psycopg2.connect(host="localhost", database="test", user="postgres", password="12345")
+# создаём объект cursor 
+cur = conn.cursor()
+cur.execute("SELECT * FROM customers")
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+сur.close()
+conn.close()
